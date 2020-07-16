61a8321b4c604d246213ea98dea11fa0997e32d2
Isaamarod <35488726+Isaamarod@users.noreply.github.com>
2020-07-16 13:02:03 +0200

    lection commit

---
+++ 02-intro-javascript/src/bases/03-objetos-literales.js
@@ -0,0 +1,13 @@
+const person={
+    name:'Isa',
+    surname:'Amaya',
+    age:26,
+    direccion: {
+        ciudad: 'Sevilla'
+    }
+};
+
+const person2 = [ ...person ];
+console.log (person);
+
+console.log (person2);
--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,8 +1,13 @@
-const person={
-    name:'Isa',
-    surname:'Amaya',
-    age:26,
-};
 
+const array = [1,2,3,4];
 
-console.log({person});
+let array2 = [ ...array , 5 ];
+
+const array3=array2.map(function(x){
+    return x+1
+});
+//array2.push(5);
+
+console.log(array);
+console.log(array2);
+console.log(array3);
