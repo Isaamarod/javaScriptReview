26a148d1a904c8fb166940f7fba7f37525fa77cb
Isaamarod <35488726+Isaamarod@users.noreply.github.com>
2020-07-16 10:26:35 +0200

    let-var explanation

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,10 @@
+//NOTE: var is not used in js since one time ago
+// const: it'll never change
+//let : it'll change
+const name = 'Isa';
+
+const second_name = 'Amaya';
+
+let age= 26;
+
+console.log(name,second_name,age)
