dd7a1daea64c1bf3fabe4e67ac8b431d4e04ea61
Isaamarod <35488726+Isaamarod@users.noreply.github.com>
2020-07-17 09:58:21 +0200

    destructuring class

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -2,25 +2,78 @@
 
   const person = {
      name: 'Isa',
-     age=26,
-     pass='hello',
+     age:26,
+     pass:'hello',
   };
 
 
 
 
-  console.log(persona.nombre);
-  console.log(persona.age);
-  console.log(persona.pass);
+  console.log(person.nombre);
+  console.log(person.age);
+  console.log(person.pass);
 
   const person2 = {
     name: 'Isa',
     age:26,
     pass:'hello',
+    role: 'Biomedical researcher',
  };
 
 
 
-  const  {name}=person2; //extract from person2 the name
+  const  {name,age,pass}=person2; //extract from person2 the name
 
-  console.log(name);
+  console.log(name,age,pass);
+
+  const returnPerson =(user) => {
+    const  {name,age,pass}=user;
+
+    console.log(name,age,pass);
+  }
+  returnPerson(person2)
+//Receive deestructuration from argument and default atb that doesn't exist
+
+const returnPerson2 =({name,edad,role}) => {
+   
+
+   console.log(name,age,role);
+ }
+ returnPerson2(person)
+
+//Receive deestructuration from argument and default atb that exist
+
+ const returnPerson3 =({name,edad,role='Researcher'}) => {
+   
+
+   console.log(name,edad,role);
+ }
+ returnPerson3(person2)
+
+
+
+ const returnPerson4 =({name,edad,role}) => {
+   
+
+   return{
+      nombreClave: pass,
+      anios: age,
+      position:{
+         lat:12.345,
+         long:89.56
+      }
+   }
+ }
+
+ const avenger = returnPerson4 (person2);
+
+ console.log(avenger)
+
+// const {nombreClave,anios,position:{lat}} = returnPerson4 (person2);
+ const {nombreClave,anios,position} = returnPerson4 (person2);
+
+ const {lat,long} = position;
+
+
+ console.log(nombreClave,anios,position);
+ console.log(lat,long);
